4faa865d1b6fe73d170a9ba8a05cc1cbe642d444
Chiru <hasenorc@gmail.com>
2025-03-29 00:10:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7508633294:AAFMGzcBeH_5HdXhtPwnTIUtvvQ6lUA0Qvo")  # Ensure correct key name
-    API_ID = int(os.environ.get("API_ID", "22581733"))  # Added key name and default value
-    API_HASH = os.environ.get("API_HASH", "1db7bdcf908100cc641c6a5276765c3d")  # Added key name for consistency
+    BOT_TOKEN = "7557612085:AAFs9uRaD8C0nZr4kBFM1ST30MzsWpKEZyA" # Ensure correct key name
+    API_ID = "26468828" # Added key name and default value
+    API_HASH ="4693513c08d1ac6af15f95b116c29478"  # Added key name for consistency
 
     AUTH_USER = os.environ.get("AUTH_USERS", "6530997270").split(',')
-    AUTH_USERS = [int(user_id) for user_id in AUTH_USER]  # Ensuring list of integers
+    AUTH_USERS = [int(7517045929) for user_id in AUTH_USER]  # Ensuring list of integers
 
-    HOST = os.environ.get("HOST", "https://api.masterapi.tech")  # Keeping HOST configurable
-    CREDIT = os.environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")  # Making CREDIT an environment variable for flexibility
+    HOST ="https://api.masterapi.tech" # Keeping HOST configurable
+    CREDIT = "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"# Making CREDIT an environment variable for flexibility
